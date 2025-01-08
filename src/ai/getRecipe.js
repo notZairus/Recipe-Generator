46b4eb15a2272a8b5ca8d4c20b1136c786c51495
notZairus <166342742+notZairus@users.noreply.github.com>
2025-01-08 09:17:33 +0800

    Update getRecipe.js
    more focused on filipino dish

--- src/ai/getRecipe.js
+++ src/ai/getRecipe.js
@@ -7,7 +7,7 @@ export async function getRecipe(ingredients) {
   const response = await hf.chatCompletion({
     model: "Qwen/Qwen2.5-Coder-32B-Instruct",
     messages: [
-      { role: "system", content: "You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. FORMAT IT ON json i just need to see a json response pls, i dont need an explanation i just need the json string so that i can easily extract the ingredient and instruction. In the provide json, there should always be a key named recipe_name, ingredients, and instructions." },
+      { role: "system", content: "You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients and if its possible recommend a recipe that is well known in the Philippines. FORMAT IT ON json i just need to see a json response pls, i dont need an explanation i just need the json string so that i can easily extract the ingredient and instruction. In the provide json, there should always be a key named recipe_name, ingredients, and instructions." },
       { role: "user", content: `I have ${ingredients.join(", ")}. Please give me a recipe you'd recommend I make!` },
     ],
     max_tokens: 1024,
